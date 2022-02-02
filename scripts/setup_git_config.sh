80fcedd6141d03b641f2dc51389762d1f20e0bf4
Dan Arad <dan1994@gmail.com>
2022-02-02 12:01:11 +0000

    Make terminal highlight word changes in git diff.

--- scripts/setup_git_config.sh
+++ scripts/setup_git_config.sh
@@ -11,3 +11,18 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.br branch
 git config --global alias.co checkout
+
+### Setup word colored diff
+if ! which diff-highlight > /dev/null 2>&1; then
+    DIFF_HIGHLIGHT_PATH=~/.local/bin/diff-highlight
+    DIFF_HIGHLIGHT_URL=https://raw.githubusercontent.com/git/git/v2.13.2/contrib/diff-highlight/diff-highlight
+    wget ${DIFF_HIGHLIGHT_URL} -O ${DIFF_HIGHLIGHT_PATH}
+    chmod +x ${DIFF_HIGHLIGHT_PATH}
+fi
+
+git config --global pager.log "diff-highlight | less"
+git config --global pager.show "diff-highlight | less"
+git config --global pager.diff "diff-highlight | less"
+
+### Extra config
+git config --global pager.branch false
